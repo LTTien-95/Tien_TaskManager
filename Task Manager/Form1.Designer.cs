754174b24dc4b282e0556c5ada72688a5da7e9ff
LTTien-95 <le.tien.hust@gmail.com>
2015-06-04 23:19:06 +0700

    Them tap tin

---
+++ Task Manager/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Task_Manager
+{
+    partial class Taskmanager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listProcess = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.killToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNumberProcess = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listProcess
+            // 
+            this.listProcess.ContextMenuStrip = this.contextMenuStrip1;
+            this.listProcess.FormattingEnabled = true;
+            this.listProcess.Location = new System.Drawing.Point(34, 32);
+            this.listProcess.Name = "listProcess";
+            this.listProcess.Size = new System.Drawing.Size(207, 225);
+            this.listProcess.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.killToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(91, 26);
+            // 
+            // killToolStripMenuItem
+            // 
+            this.killToolStripMenuItem.Name = "killToolStripMenuItem";
+            this.killToolStripMenuItem.Size = new System.Drawing.Size(90, 22);
+            this.killToolStripMenuItem.Text = "Kill";
+            this.killToolStripMenuItem.Click += new System.EventHandler(this.killToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Process";
+            // 
+            // lblNumberProcess
+            // 
+            this.lblNumberProcess.AutoSize = true;
+            this.lblNumberProcess.Location = new System.Drawing.Point(88, 273);
+            this.lblNumberProcess.Name = "lblNumberProcess";
+            this.lblNumberProcess.Size = new System.Drawing.Size(13, 13);
+            this.lblNumberProcess.TabIndex = 2;
+            this.lblNumberProcess.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 306);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Kill";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Taskmanager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 352);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblNumberProcess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listProcess);
+            this.Name = "Taskmanager";
+            this.Text = "Task Manager";
+            this.Load += new System.EventHandler(this.Taskmanager_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNumberProcess;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem killToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Task Manager/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics; //Cung cấp truy cập đến các quá trình địa phương và từ xa và cho phép bạn bắt đầu và ngừng quá trình hệ thống địa phương.
+
+namespace Task_Manager
+{
+    public partial class Taskmanager : Form
+    {
+        public Taskmanager()
+        {
+            InitializeComponent();
+        }
+        Process[] process;   // Khởi tạo mảng null process
+
+        //Lấy các process đang xử lý trong máy đưa vào list box
+
+        private void GetProcess()
+        {
+            process = Process.GetProcesses();
+            if (int.Parse(lblNumberProcess.Text) != process.Length)
+            {
+                listProcess.Items.Clear();         //Xoá sạch listbox 
+                for (int i = 0; i < process.Length; i++)
+                {
+                    listProcess.Items.Add(process[i].ProcessName);
+                }
+                lblNumberProcess.Text = process.Length.ToString();  // Đếm số process
+            }
+        }
+
+        private void KillProcess(int index)     //Tạo 1 biến int index: chỉ mục
+        {
+            process[index].Kill();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetProcess();
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            KillProcess(listProcess.SelectedIndex);  // Kill Process trong index được chọn trong listbox
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetProcess();
+        }
+        //Tự động cập nhật số Process trong 1s
+
+
+
+        private void killToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            KillProcess(listProcess.SelectedIndex);
+        }
+        //Thêm chức năng mới có nút chọn phải Kill
+
+    }
+}
